dc88474a7a49d1b6a67d146c836c0387750409f1
maniebra <mani.ebra@gmail.com>
2025-07-10 11:19:30 +0330

    add user model and db migration

--- Source/Domain/Entities/Auth/User.cs
+++ Source/Domain/Entities/Auth/User.cs
@@ -1,22 +1,49 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace DotNetCleanTemplate.Source.Domain.Entities.Auth;
 
 public class User
 {
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public required Guid id;
+    [Key]
+    public required Guid id { get; init; } = new Guid();
+
+    [Required, MaxLength(64)]
+    public required string Username { get; set; }
 
     [Required]
-    public required string Username;
-
-    [Required]
-    public required string Password;
+    public required string Password { get; set; }
         
     [MaxLength(64)]
-    public required string FirstName;
+    public required string FirstName { get; set; }
 
     [MaxLength(64)]
-    public required string LastName;
+    public required string LastName { get; set; }
+
+    [MaxLength(64), EmailAddress]
+    public required string Email { get; set; }
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public required DateTime CreatedAt { get; init; }
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public required DateTime? UpdatedAt { get; set; }
+
+    public required Boolean IsAdmin  { get; set; } = false;
+    public required Boolean IsStaff  { get; set; } = false;
+    public required Boolean IsBanned { get; set; } = false;
+
+    public static void Configure(EntityTypeBuilder<User> b)
+    {
+        b.ToTable("Users");
+        
+        b.HasIndex(u => u.Username)
+            .IsUnique();
+
+        b.HasIndex(u => u.Email)
+            .IsUnique();
+    }
 }
---
+++ Source/Persistence/Migrations/20250710074914_InitialUserCreate.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotNetCleanTemplate.Source.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialUserCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Username = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    FirstName = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    LastName = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    Email = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    IsAdmin = table.Column<bool>(type: "boolean", nullable: false),
+                    IsStaff = table.Column<bool>(type: "boolean", nullable: false),
+                    IsBanned = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Username",
+                table: "Users",
+                column: "Username",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
